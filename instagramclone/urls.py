9a584cf8c12f4a6f2d713b681d634ab742e5e23f
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-10-30 16:38:42 -0500

    Starting Project InstagramClone

---
+++ instagramclone/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    return HttpResponse('Hello, world!')
+
+
+urlpatterns = [
+    path('hello-world/', hello_world)
+]
