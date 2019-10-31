a9a85bf1203eb75c1408b517dd104ea3f9ce16ec
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-10-31 17:05:34 -0500

    added posts List

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from instagramclone import views
+from instagramclone import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world),
-    path('sorted/', views.sorted_integers),
-    path('hi/<str:name>/<int:age>/', views.say_hi)
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sorted_integers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
+    path('posts/', posts_views.list_post),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from datetime import datetime
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Facundo Esculapio',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'Artemio Abundo',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Vilorio Carolino',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+def list_post(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
