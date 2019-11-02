ddaa3a01c096698b95b9f4f634d124ac32403212
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-11-01 19:34:03 -0500

    user class created and admin site included

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birth_date = models.DateField(blank=True, null=True)
+
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateField(auto_now=True)
+
+    is_admin = models.BooleanField(default=False)
