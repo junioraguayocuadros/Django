605d59ec00e06ce97b04aa5e56a7c5eacb23ebca
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-11-01 21:36:42 -0500

    added User + Profile into admin website

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 from users.models import Profile
 
@@ -13,3 +15,34 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
 
     list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        ('Extra Info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+             )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
