1c5dda7293e62ac96731657161dc8a5b9fdd9587
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-11-01 20:53:11 -0500

    Modified users.admin for register with a class

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from users.models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
+
+    list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
