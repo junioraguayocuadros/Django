32f1cf322001b084cfe26886b7460a2eb7189789
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-07 11:21:38 -0500

    Added user detail view class

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from users import views
 urlpatterns = [
     path(
         route='<str:username>/',
-        view=TemplateView.as_view(template_name='users/detail.html'),
+        view=views.UserDetailView.as_view(),
         name='detail'
     ),
     path(
--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
 from users.forms import ProfileForm, SignupForm
 
+
+class UserDetailView(TemplateView):
+    template_name = 'users/detail.html'
 
 @login_required
 def update_profile(request):
