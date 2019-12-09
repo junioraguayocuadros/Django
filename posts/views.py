0bb9ca37e4259b97f711ca266bba667d00688485
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2019-12-08 22:58:56 -0500

    Added created post view

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from datetime import datetime
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 from posts.forms import PostForm
 from posts.models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
 
 
 class PostDetailView(LoginRequiredMixin, DetailView):
@@ -21,20 +20,14 @@ class PostFeedView(LoginRequiredMixin, ListView):
     context_object_name = 'posts'
 
 
-@login_required
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
+class CreatePostView(LoginRequiredMixin, CreateView):
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
 
-    return render(request=request,
-                      template_name='posts/new.html',
-                      context={
-                          'form': form,
-                          'user': request.user,
-                          'profile': request.user.profile
-                      })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
+
